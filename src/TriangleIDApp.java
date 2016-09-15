37d594e948616ef7cc46f861ac4d077d0267bece
Shubin, Andrew <andrewbshubin@gmail.com>
2016-09-15 01:53:22 -0700

    Finished v1.0.0

--- src/TriangleIDApp.java
+++ src/TriangleIDApp.java
@@ -1,7 +1,7 @@
 /*
  * TriangleIDApp.java
  *
- * Version 1.0.0.0
+ * Version 1.0.0
  *
  * Created 9/11/16 by Andrew Shubin
  */
@@ -16,7 +16,11 @@ public class TriangleIDApp {
         gui = new TriangleUI();
     }
 
-    public static String idTriangle(double a, double b, double c) {
+    public static String idTriangle(double aRaw, double bRaw, double cRaw) {
+        double a = Math.abs(aRaw);
+        double b = Math.abs(bRaw);
+        double c = Math.abs(cRaw);
+
         // Test for triangle validity
         if (invalidTriangle(a, b, c)) {
             return "This is not a valid triangle.";
@@ -36,9 +40,23 @@ public class TriangleIDApp {
         return "This triangle is " + type + ".";
     }
 
+    public static boolean inputIsValid(String a, String b, String c) {
+        return isDouble(a) && isDouble(b) && isDouble(c);
+    }
+
     private static boolean invalidTriangle(double a, double b, double c) {
         double max = Math.max(a, Math.max(b, c));
-        return max < (a + b + c - max); // Triangle if longest side is shorter
+        return max >= (a + b + c - max); // Triangle if longest side is shorter
                                         // than the sum of the other two sides
+    }
+
+    // isDouble method from... http://stackoverflow.com/questions/3133770/how-to-find-out-if-the-value-contained-in-a-string-is-double-or-not
+    private static boolean isDouble(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
--- src/TriangleUI.java
+++ src/TriangleUI.java
@@ -1,18 +1,114 @@
 /*
  * TriangleUI.java
  *
- * Version 1.0.0.0
+ * Version 1.0.0
  *
  * Created 9/9/16 by Andrew Shubin
  */
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.Arc2D;
+import javax.swing.*;
+
 public class TriangleUI {
+
+    /*
+     *   Code heavily influenced by sample code at:
+     *   https://www.tutorialspoint.com/swing/swing_quick_guide.htm
+     */
+
+    private JFrame mainFrame;
+    private JLabel headerLabel;
+    private JLabel statusLabel;
+    private JPanel inputPanel;
     
     public TriangleUI() {
         prepareGUI();
+        showGUI();
     }
     
     private void prepareGUI() {
-        //TODO: all GUI coding.
+        mainFrame = new JFrame("A Triagle Identifier");
+        mainFrame.setSize(700, 300);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setLayout(new GridLayout(3,1));
+
+        headerLabel = new JLabel("", JLabel.CENTER);
+        statusLabel = new JLabel("", JLabel.CENTER);
+
+        statusLabel.setSize(275,50);
+
+        mainFrame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
+
+        inputPanel = new JPanel();
+        inputPanel.setLayout(new GridLayout(4,3));
+
+        mainFrame.add(headerLabel);
+        mainFrame.add(inputPanel);
+        mainFrame.add(statusLabel);
+        mainFrame.setVisible(true);
     }
-    
+
+    private void showGUI() {
+        headerLabel.setText("Welcome. Please enter 3 triangle side-lengths:");
+
+        JButton submitButton = new JButton("Identify!");
+
+        JTextField sideA = new JTextField();
+        JTextField sideB = new JTextField();
+        JTextField sideC = new JTextField();
+
+        JLabel labelA = new JLabel("Side A: ", JLabel.RIGHT);
+        JLabel labelB = new JLabel("Side B: ", JLabel.RIGHT);
+        JLabel labelC = new JLabel("Side C: ", JLabel.RIGHT);
+
+        submitButton.setActionCommand("submit");
+
+        inputPanel.add(labelA);
+        inputPanel.add(sideA);
+        inputPanel.add(new JLabel("")); // These are placeholders
+        inputPanel.add(labelB);
+        inputPanel.add(sideB);
+        inputPanel.add(new JLabel(""));
+        inputPanel.add(labelC);
+        inputPanel.add(sideC);
+        inputPanel.add(new JLabel(""));
+        inputPanel.add(new JLabel(""));
+        inputPanel.add(submitButton);
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String command = e.getActionCommand();
+
+                if (command.equals("submit")) {
+                    String aString = sideA.getText();
+                    String bString = sideB.getText();
+                    String cString = sideC.getText();
+
+                    if (TriangleIDApp.inputIsValid(aString, bString, cString)) {
+                        String id = TriangleIDApp.idTriangle(Double.parseDouble(aString),
+                                                                Double.parseDouble(bString),
+                                                                Double.parseDouble(cString));
+                        statusLabel.setText(id);
+                    }
+                    else {
+                        statusLabel.setText("The input is invalid. Please use numbers only.");
+                    }
+                }
+                else {
+                    statusLabel.setText("App encountered an error... :(");
+                }
+            }
+        });
+
+        mainFrame.setVisible(true);
+    }
+
 }
